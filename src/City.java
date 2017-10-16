b020745b79cf68bfbc0229ae44482b27154f0329
killghost15 <killghost15@gmail.com>
2017-10-16 23:39:24 +0100

    fixing city behaviour and reward calculation

--- src/City.java
+++ src/City.java
@@ -7,7 +7,7 @@ import jade.lang.acl.ACLMessage;
 public class City extends Agent{
 	private AID id;
 	private String name;
-	private float min,x1; //x1 is water withdrawn for the city
+	private float Q1,minC,minD,x1,a1,b1,c1; //minC is water withdrawn for the city,minD is minimum water for dam, a,b,c are constants of the problem
 	protected void setup(){
 		
 		Object[] args = getArguments();
@@ -16,7 +16,13 @@ public class City extends Agent{
 			 id = new AID("City"+name, AID.ISLOCALNAME);
 			 
 			 
-			 min=(float)args[1];
+			 minC=(float)args[1];
+			 x1=minC;
+			 minD=(float)args[2];
+			 a1=(float)args[3];
+			 b1=(float)args[4];
+			 c1=(float)args[5];
+			 Q1=(float)args[6];
 			 
 			
 			 //n está operacional ainda, só em conceito
@@ -38,14 +44,20 @@ public class City extends Agent{
 		public void action() {
 			// TODO Auto-generated method stub
 			// envia-a o 
+			//sending x1 and F so it is possible to continue shared problem and calculate the Reward from sum of penalty function and sum of fobjectives
+			/*  R =
+			SUM i=1 até X=6 de fi(xi) 
+			-
+			SUM j=1 até X=N de C(|hj + 1|lj )*/
 			
 			ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
-			msg.addReceiver(new AID("City"+name, AID.ISLOCALNAME));
+			msg.addReceiver(new AID("Dam", AID.ISLOCALNAME));
 			msg.setLanguage("English");
 			msg.setOntology("Value sharing");
-			msg.setContent(""+x1);
+			msg.setContent(""+x1+" "+fobjectivecity()+" "+fpenalty());
 			send(msg);
 			ACLMessage answer = myAgent.receive();
+			//on reception from Eco2 should restart and increment x1
 			if (answer != null) {
 				if(state.equals("start")){
 					
@@ -72,5 +84,26 @@ public class City extends Agent{
 		
 	}
 	
-
+	public float fobjectivecity(){
+		return a1*x1*x1+b1*x1+c1;
+	}
+	public float fpenalty(){
+		float res=0;
+		if(minC-x1 <=0 && minD-Q1+x1<=0){
+			return res;
+		}
+		else{
+			if(minC-x1>0){
+				res=1000*minC-x1;
+			}
+			if(minD-Q1+x1>0){
+				res+=100*(minD-Q1+x1);
+			}
+			return res;
+		}
+		
+	
+	}
+	
+	
 }
